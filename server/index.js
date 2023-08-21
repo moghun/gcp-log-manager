0cf42a85eb2d363e90bb47886ed8f1e4ce3bc4d6
Muhammed Orhun Gale <morhun@sabanciuniv.edu>
2023-08-22 02:37:24 +0300

    getAnalytics queries are implemented

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PubSub } = require("@google-cloud/pubsub");
 const { BigQuery } = require("@google-cloud/bigquery");
+const { time } = require("console");
 
 const app = express();
 const port = process.env.PORT || 3000;
 const projectId = process.env.PROJECT_ID;
 const keyFilename = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 const topicName = process.env.TOPIC_NAME;
+const datasetName = process.env.BIGQUERY_TABLE_NAME;
 const script_path = "../scripts";
 
 const pubsub = new PubSub({
@@ -78,7 +80,85 @@ app.post("/api/postLog", async (req, res) => {
 });
 
 // Fetch and display analytics data from BigQuery
-app.get("/api/getAnalytics", async (req, res) => {});
+app.get("/api/getAnalytics", async (req, res) => {
+  try {
+    //Get total users from BigQuery
+    const totalUsersQuery = `
+        SELECT COUNT(DISTINCT user_id) AS total_users
+        FROM ${datasetName};
+        `;
+
+    // Get daily active users from BigQuery
+    const queryDailyActiveUsers = `
+      SELECT
+        DATE(event_time) AS date,
+        COUNT(DISTINCT user_id) AS distinct_users
+      FROM ${datasetName}
+      GROUP BY date
+      ORDER BY date DESC;
+      `;
+
+    // Get daily new users from BigQuery
+    const queryDailyNewUsers = `
+      SELECT
+        DATE(event_time) AS date,
+        COUNT(DISTINCT user_id) AS daily_new_users
+      FROM ${datasetName}
+      WHERE event_name = 'sign_up'
+      GROUP BY date
+      ORDER BY date DESC;
+      `;
+
+    // Get a list of "session_start" - "session_end" couples for a unique user and session time from BigQuery
+    // Then calculate the average session time for each day on the server side
+    const queryAvgTime = `
+      WITH SessionData AS (
+        SELECT
+          DATE(event_time) AS date,
+          user_id,
+          type,
+          session_id,
+          event_time AS session_time
+        FROM
+          ${datasetName}
+        WHERE
+          type IN ('session_start', 'session_end')
+      )
+      SELECT
+        date,
+        ARRAY_AGG(STRUCT(session_duration_seconds)) AS user_sessions
+      FROM (
+        SELECT
+          date,
+          TIMESTAMP_DIFF(MAX(CASE WHEN type = 'session_end' THEN session_time END), MIN(CASE WHEN type = 'session_start' THEN session_time END), SECOND) AS session_duration_seconds
+        FROM SessionData
+        GROUP BY date, user_id, session_id
+      )
+      GROUP BY
+        date
+      ORDER BY
+        date DESC;
+    `;
+
+    // Execute queries
+    const [totalUsers] = await bigquery.query(totalUsersQuery);
+    const [dailyActiveUsers] = await bigquery.query(queryDailyActiveUsers);
+    const [dailyNewUsers] = await bigquery.query(queryDailyNewUsers);
+    const [avgTime] = await bigquery.query(queryAvgTime);
+
+    const dailyStats = [];
+    // Combine all data into a single object to be returned as response
+    const activtyRecords = {
+      total_users: totalUsers[0] != null ? totalUsers[0].total_users : 0,
+      daily_stats: dailyStats,
+    };
+
+    res.status(200).json(activtyRecords);
+  } catch (error) {
+    console.error(`Error fetching analytics data: ${error}`);
+    res.status(500).send("Error fetching analytics data");
+  }
+});
 
 // Run Express.js server to listen log and analytics requests
 app.listen(port, () => {
